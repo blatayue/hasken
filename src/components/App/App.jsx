b3da36d1950973344401c178a9532a5c32ae94b8
Andrew-Morris <blatadude@gmail.com>
2017-10-28 22:09:24 -0700

    Links Component added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import style from './App.scss'
 import Navbar from '../Navbar/Navbar'
+import Links from '../Links/Links'
 export default class App extends React.Component {
     render () {
         return (
             <div className={style.mainContainer}>
                 <Navbar />
+                <Links />
             </div>
         )
     }
---
+++ src/components/Links/Links.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import style from './Links.scss'
+import Lazyload from 'react-lazyload'
+const Links = () => (
+    <div className={style.link}>
+        <a href="https://reddit.com" className={style.link__reddit}>
+            <Lazyload>
+                <img src={require('../../images/snoo.png')} height='20px'/>
+            </Lazyload>
+            <p>Reddit</p>
+        </a>
+        <a href="https://medium.com" className={style.link__medium}>
+            <Lazyload>
+                <img src={require('../../images/medium.jpg')} height='20px'/>
+            </Lazyload>
+            <p>Medium</p>
+        </a>
+        <a href="https://youtube.com" className={style.link__youtube}>
+            <Lazyload>
+                <img src={require('../../images/play.png')} height='20px'/>
+            </Lazyload>
+            <p>YouTube</p>
+        </a>
+    </div>
+)
+
+export default Links
