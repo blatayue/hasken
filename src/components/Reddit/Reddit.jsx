200d19d9f6e2e939135b9289a8acb38c4c9274c0
Andrew-Morris <blatadude@gmail.com>
2017-10-28 22:22:13 -0700

    began work on reddit component

--- src/components/Reddit/Reddit.jsx
+++ src/components/Reddit/Reddit.jsx
@@ -1,4 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
 class Reddit extends React.Component {
-    
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
 }
+
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps)(Reddit)
